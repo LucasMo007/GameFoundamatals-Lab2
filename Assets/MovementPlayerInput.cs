910f786fc1b65af2c99c135b5ac6a92b7be52566
LucasMo007 <easoncoldplay2@gmail.com>
2025-02-06 17:07:01 -0500

    add speed and make the speed same all the time
    When W and D are held at the same time ,we keep the speed. magnitude at 1

--- Assets/MovementPlayerInput.cs
+++ Assets/MovementPlayerInput.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MovementPlayerInput : MonoBehaviour
@@ -17,11 +18,23 @@ public class MovementPlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float XInput = Input.GetAxis("Horizontal");
-        float YInput = Input.GetAxis("Vertical");
+        float XInput = Input.GetAxisRaw("Horizontal");
+        float YInput = Input.GetAxisRaw("Vertical");
         float DT= Time.deltaTime;
-        inputvector = new Vector3(XInput, YInput, 0);
-        
-        transform.position = transform.position + inputvector * DT;
+        inputvector = new Vector3 (XInput, YInput, 0);
+        //if W is held alone,this vector will be (0,1,0),
+        //for a velocity of (0,speed,0)
+        //if W and D are held ,this vector will be(1, 1, 0),
+        //for a velocity of(speed, speed, 0)
+        if (inputvector.x == 0 && inputvector.y == 0)
+        {
+
+        }
+        else
+        {
+            inputvector = inputvector / inputvector.magnitude;
+        }
+        //Debug.Log(inputvector.magnitude);
+        transform.position = transform.position + inputvector * speed * DT;
     }
 }
