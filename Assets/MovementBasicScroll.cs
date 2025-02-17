1c53ec8baf29deef3329a25ae0c751efa0b8db36
LucasMo007 <easoncoldplay2@gmail.com>
2025-02-16 23:29:04 -0500

    Update MovementBasicScroll.cs

--- Assets/MovementBasicScroll.cs
+++ Assets/MovementBasicScroll.cs
@@ -18,6 +18,8 @@ public class MovementBasicScroll : MonoBehaviour
     {    //how long did last frame take?at 60 fps =0.01666
         float DT = Time.deltaTime;
         //move every frame by x units
+        //if 60 fps ,the rock movement 1m/s *1/60s * 60 =1m
         transform.position = transform.position+new Vector3(speed, 0,0)*DT;
+
     }
 }
