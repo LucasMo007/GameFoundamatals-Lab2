4f6c0fcd2e70b9470cce0bbc77d6b68cab487538
LucasMo007 <easoncoldplay2@gmail.com>
2025-02-26 09:24:21 -0500

    Destroy ,3 shooting pattern and PowerUp
    1.Bullets are  destroyed by distance
    2.change shooting mode by selected numbers on keyboard
    3player come over and pick up the powerup ,change 3 shooting mode

--- Assets/MovementBasicScroll.cs
+++ Assets/MovementBasicScroll.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -7,30 +7,48 @@ public class MovementBasicScroll : MonoBehaviour
 {
     public float speed = 1f;
     public float range = 18;
-
-    Vector3 StartPos;
+    public float distancetravelled = 0;
+    //Vector3 StartPos;
 
     // Start is called before the first frame update
     private void Start()
     {
-        StartPos = transform.position;
+        //StartPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
-    {    //how long did last frame take?at 60 fps =0.01666 ?????????
+    {    //how long did last frame take?at 60 fps =0.01666 两帧时间的时间间隔是0.01666。
+
+
         float DT = Time.deltaTime;
+
+        Vector3 ForwardVector = transform.up;
+        
+
         //move every frame by x units
         //if 60 fps ,the rock movement 1m/s *1/60s * 60 =1m
-        transform.position = transform.position+new Vector3(speed, 0,0)*DT;
 
-        Vector3 Currentpos = transform.position;
-        Vector3 displacement = Currentpos - StartPos;
+        transform.position = transform.position + ForwardVector * speed * DT;
 
-        if(displacement.magnitude > range )
+        distancetravelled += speed * DT;
+
+        //Vector3 Currentpos = transform.position;
+        //Vector3 displacement = Currentpos - StartPos;
+
+        //First way to destroy the object
+        if (distancetravelled > range)
         {
             Destroy(gameObject);
         }
+
+        //Second way to destroy the object
+        /*if(displacement.magnitude > range )
+        {
+            Destroy(gameObject);
+        }*/
+
+        //third way to destroy the object
         /*if(transform.position.x > 10 || transform.position.x <-10)
         {
             Destroy(gameObject);
--- Assets/PlayerShooting.cs
+++ Assets/PlayerShooting.cs
@@ -1,37 +1,158 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
-{       public GameObject projectileprefab;
-        public bool istriggeredown;
-        public float timeuntilreloaded;
-        public float firerate =1;// shots every second
+{
+    public GameObject projectileprefab;
+    public bool istriggeredown;
+    public float timeuntilreloaded = 0;
+    public float firerate = 3;// shots every second
 
+    public ShootModes shootMode = ShootModes.Basic;
 
+    //public GameObject spreadPowerup;
 
-    // Update is called once per frame
-    void Update()
+    public enum ShootModes
     {
-       /* if (Input.GetButton ("shoot")) ;
-        Instantiate(projectileprefab,transform.position,transform.rotation);*/
-      
-            istriggeredown = Input.GetButton ("shoot");
+        Basic = 0,
+        Spread,
+        Rapid
+    }
+        private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Powerup")
+        {   
+            shootMode = ShootModes.Spread;
+            Destroy(collision.gameObject);
+        }
+    }
 
+/*if(timeuntilreloaded <= 0)
+{
+    timeuntilreloaded = 0;
+}*/
+
+
+// Update is called once per frame
+void Update()
+{
+
+    // did we touch a powerup?
+    // test distance between player and powerup
+
+    /*Vector3 PowerupPos = spreadPowerup.transform.position ;
+    Vector3 PlayerPos = transform.position;
+
+    float distance = Vector3.Distance(PlayerPos,PowerupPos);
+
+    if (distance < 0.5f)
+    {
+        // we touched the powerup
+        // do something
+        shootMode = ShootModes.Spread;
+        Destroy(spreadPowerup);
+        spreadPowerup = null;
+    }
+    */
+
+
+
+
+    /* if (Input.GetButton ("shoot")) ;
+     Instantiate(projectileprefab,transform.position,transform.rotation);*/
+
+    istriggeredown = Input.GetButton("shoot");
+
+    if (Input.GetKeyDown(KeyCode.Alpha1))
+    {
+            shootMode = ShootModes.Basic;
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha2))
+    {
+            shootMode = ShootModes.Spread;
+    }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            shootMode = ShootModes.Rapid;
+        }
+        if (shootMode == ShootModes.Basic)
+    {
+     BasicShootingBehaviour();
+    }
+    else if (shootMode == ShootModes.Spread)
+    {
+    SpreadShootingBehaviour();
+    }
+        else if (shootMode == ShootModes.Rapid)
+        {
+            RapidShootingBehaviour();
+        }
+
+        //if (Input.GetKeyDown(KeyCode.Alpha1))
+        //{
+        //    ShootingMode = 1;
+        //}
+        //if (Input.GetKeyDown(KeyCode.Alpha2))
+        //{
+        //    ShootingMode = 2;
+        //}
+
+        //if (ShootingMode == 1)
+        //{
+        //    BasicShootingBehaviour();
+        //}
+        //else if (ShootingMode == 2)
+        //{
+        //    SpreadShootingBehaviour();
+        //}
+
+
+    }
+    void RapidShootingBehaviour()
+    {
+        if (istriggeredown && timeuntilreloaded <= 0)
+        {
+            Instantiate(projectileprefab, transform.position, transform.rotation);
+            float secondsPerShot = 1 / firerate/5;
+            timeuntilreloaded = secondsPerShot;
+        }
+        timeuntilreloaded -= Time.deltaTime;
+        if (timeuntilreloaded <= 0)
+        {
+            timeuntilreloaded = 0;
+        }
+    }
+    void BasicShootingBehaviour()
+    {
         if (istriggeredown && timeuntilreloaded <= 0)
         {
             Instantiate(projectileprefab, transform.position, transform.rotation);
             float secondsPerShot = 1 / firerate;
             timeuntilreloaded = secondsPerShot;
         }
-
         timeuntilreloaded -= Time.deltaTime;
-        /*if(timeuntilreloaded <= 0)
+        if (timeuntilreloaded <= 0)
         {
             timeuntilreloaded = 0;
-        }*/
-
-
+        }
+    }
+    void SpreadShootingBehaviour ()
+    {
+        if (istriggeredown && timeuntilreloaded <= 0)
+        {
+            Instantiate(projectileprefab, transform.position, transform.rotation);
+            Instantiate(projectileprefab, transform.position, transform.rotation * Quaternion.Euler(0, 0, 10));
+            Instantiate(projectileprefab, transform.position, transform.rotation * Quaternion.Euler(0, 0, -10));
+            float secondsPerShot = 1 / firerate;
+            timeuntilreloaded = secondsPerShot;
+        }
+        timeuntilreloaded -= Time.deltaTime;
+        if (timeuntilreloaded <= 0)
+        {
+            timeuntilreloaded = 0;
+        }
     }
 }
